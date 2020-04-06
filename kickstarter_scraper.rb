006f736a2aa21f347b5a3ed804525e71fcd368ef
Yarvin Hernandez <yarhumher@hotmail.com>
2020-04-06 23:14:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,12 +6,12 @@
 
  def create_project_hash
    projects = {}
-  
+
    kickstarter.css("li.project.grid_4").each do |project|
      title = project.css("h2.bbcard_name strong a").text
      projects[title.to_sym] = {}
    end
-  
+
    # return the projects hash
    projects
  end
