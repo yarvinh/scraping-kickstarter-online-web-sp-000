b27809f53084a0c4dc8c6061e10cb3c74b537e7e
Yarvin Hernandez <yarhumher@hotmail.com>
2020-04-06 23:12:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,10 +5,17 @@
 # require_relative 'fictures/Kickstarter.html'
 
  def create_project_hash
-      #  Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      html = File.read('fixtures/kickstarter.html')
-      kickstarter = Nokogiri::HTML(html)
-      #kickstarter.css("h2.bbcard_name strong a").text
-       #binding.pry
+   html = File.read('fixtures/kickstarter.html')
+kickstarter = Nokogiri::HTML(html)
+
+projects = {}
+
+# Iterate through the projects
+kickstarter.css("li.project.grid_4").each do |project|
+projects[project] = {}
+end
+
+# return the projects hash
+projects
  end
 create_project_hash
